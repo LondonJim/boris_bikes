dd5427304d227817d8c1832bad426d98e764639d
Freddie <fredjlawson@gmail.com>
2018-08-08 16:56:44 +0100

    just finished .12, working tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,25 @@
 class DockingStation 
     
+
+    # def release_bike
+    #     if @bikes == nil
+    #         raise
+    #     else
+    #         @bikes
+    #     end
+    # end
+
     def release_bike
-        Bike.new
+        fail 'No bikes available' unless @bikes
+        @bikes
     end
+
+
+    def dock(bike_to_be_docked)
+        @bikes = bike_to_be_docked
+    end
+
+
+    attr_reader :bikes
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
@@ -8,14 +9,38 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
     end
 
-    it 'creates a new object of the .bike class when #release_bike is called on it' do 
-    expect(subject.release_bike.working?).to eq true
-    end
 
     # the below test is from the walkthrough - maybe a nicer way of writing the above test? 
     it 'releases working bikes' do
-        bike = subject.release_bike
-        expect(bike).to be_working
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+    end
+
+
+    it "#dock method takes an argument" do 
+        is_expected.to respond_to(:dock).with(1).argument 
+    end
+
+    it { is_expected.to respond_to(:bikes) }
+
+    it 'returns docked bikes' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bikes).to eq bike
+    end
+
+    it "if has #dock called on it then expect #bikes to return the bike" do
+        bike2 = Bike.new
+        station = DockingStation.new
+        station.dock(bike2)
+        expect(station.bikes).to eq(bike2)
+    end
+
+
+    it "#release_bikes fails if docking station holds no bike" do
+        # if subject.bikes == nil
+        expect { subject.release_bike }.to raise_error 'No bikes available'
     end
 
 end
